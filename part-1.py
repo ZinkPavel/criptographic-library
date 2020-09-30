6d698a2fe2007caead4125d2054431f10acccd56
ZinkPavel <tsynk.p@gmail.com>
2020-09-30 19:32:43 +0700

    Added fast modulo exponential method

--- part-1.py
+++ part-1.py
@@ -9,3 +9,18 @@ def isPrime(n):
     for num in range(2, math.floor(math.sqrt(n))):
         if (n % num == 0): return False
     return True
+
+def fastModuloExponentiation(a, x, p):
+    if x < 1 or x >= p: sys.exit('ERROR. [X] Must be from range (1, 2, ... , p-1)')
+    
+    result = 1
+    t = math.floor(math.log2(x))
+    tmp = a
+    for num in range(t + 1):
+        if num > 0: tmp *= tmp % p
+
+        if (x % 2) == 1:
+            result *= tmp
+        x = x // 2
+
+    return result % p 
