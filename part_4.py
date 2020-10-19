8469adbcd90eecd6612f8d65e4ff3b3a88aeae74
ZinkPavel <tsynk.p@gmail.com>
2020-10-19 18:29:30 +0700

    Added Player class

--- part_4.py
+++ part_4.py
@@ -1,15 +1,20 @@
 import part_1
 
 
+class Player:
+    def __init__(self, name = ''):
+        self.name = name
+        self.card = []
+
+
 class PokerRoom:
     def __init__(self, num_players):
-        self.num_players = num_players
+        self.players = [Player() for i in range(num_players)]
         self.card_on_desk = []
 
 
 def main():
     barnaul = PokerRoom(5)
-
 
 if __name__ == "__main__":
     main()
