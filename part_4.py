12dda5f3ef64998d2f195b6e798d2e963a699fba
ZinkPavel <tsynk.p@gmail.com>
2020-10-19 18:15:51 +0700

    Added PokerRoom class

---
+++ part_4.py
@@ -0,0 +1,15 @@
+import part_1
+
+
+class PokerRoom:
+    def __init__(self, num_players):
+        self.num_players = num_players
+        self.card_on_desk = []
+
+
+def main():
+    barnaul = PokerRoom(5)
+
+
+if __name__ == "__main__":
+    main()
