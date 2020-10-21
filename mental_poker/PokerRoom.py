7f84aa233a7d5c4a931057258e63a4ce3ab2fcfd
ZinkPavel <tsynk.p@gmail.com>
2020-10-21 12:16:26 +0700

    Added .give_card

--- mental_poker/PokerRoom.py
+++ mental_poker/PokerRoom.py
@@ -1,6 +1,9 @@
 import random
+import sys
 
 import part_1
+import part_3
+
 from mental_poker.Player import Player
 from mental_poker.CardDeck import CardDeck
 from mental_poker.Consts import SALT_P, SALT_G
@@ -8,16 +11,33 @@ from mental_poker.Consts import SALT_P, SALT_G
 
 class PokerRoom:
     def __init__(self, num_players):
-        self.__secret_key = random.randint(1, 2 ** 10)
-        self.open_key = part_1.fast_modulo_exponentiation(SALT_G, self.__secret_key, SALT_P)
-        self.z = []
+        self.k = random.randint(1, SALT_P - 1)
+        self.open_key_1 = part_1.fast_modulo_exponentiation(SALT_G, self.k, SALT_P)  # link
+        self.keys = []  # link
+        # self.open_keys_2 = []
 
-        self.players = [Player(self.open_key, 'Player-' + str(i)) for i in range(num_players)]
+        self.players = [Player(self.open_key_1, 'Player-' + str(i)) for i in range(num_players)]
         self.deck = CardDeck().deck
         self.card_on_desk = []
 
-        for i in range(0, len(self.players)):
-            self.z.append(part_1.fast_modulo_exponentiation(self.players[i].open_key, self.__secret_key, SALT_P))
+        for i in range(0, len(self.players)):  # make pair
+            self.keys.append(part_1.fast_modulo_exponentiation(self.players[i].open_key_1, self.k, SALT_P))
+            # self.open_keys_2.append(part_1.fast_modulo_exponentiation(SALT_G, self.keys[i], SALT_P))
+        self.check()
+
+    def check(self):
+        if 1 > self.k > SALT_P - 2:
+            sys.exit(1)
+
+    def give_card(self, player):
+        card = bytearray(str(self.deck.pop()), 'ascii')
+        r = part_1.fast_modulo_exponentiation(SALT_G, self.k, SALT_P)
+        e = [0] * len(card)
+
+        for i in range(0, len(card)):
+            e[i] = card[i] * part_1.fast_modulo_exponentiation(player.open_key_2, self.k, SALT_P) % SALT_P
+
+        # player.take_card(r, e)
 
     def distribution(self):
         for player in self.players:
