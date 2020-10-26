49956fbaeb8677aaf8704df40465e1345d388d82
ZinkPavel <tsynk.p@gmail.com>
2020-10-26 17:07:01 +0700

    Add proof isomorphism

--- hamiltonian_cycle/crypt.py
+++ hamiltonian_cycle/crypt.py
@@ -24,3 +24,21 @@ class GraphRSA:
                 self.coded[i][j] += self.graph.graph_h[i][j] + random.randint(1, self.graph.n) * 10
                 self.encrypted[i][j] += fme(self.coded[i][j], 3, self.n)
 
+    def proof_isomorphism(self):
+        for i in range(0, len(self.graph.graph_h)):
+            for j in range(0, len(self.graph.graph_h[i])):
+                tmp = fme(self.coded[i][j], 3, self.n)
+
+                if not tmp == self.encrypted[i][j]:
+                    raise ValueError('Error:' + str(tmp) + '!=' + str(self.encrypted[i][j]))
+
+        result = np.zeros((8, 8))
+        for i in range(0, len(self.graph.graph_h)):
+            for j in range(0, len(self.graph.graph_h[i])):
+                result[i][j] = self.coded[i][j] % 10
+
+        for elem in self.graph.data:
+            if not result[self.graph.new_indices[elem[0]]][self.graph.new_indices[elem[1]]] == self.graph.graph_g[elem[0]][elem[1]]:
+                raise ValueError('Error: proof isomorphism')
+            if not result[self.graph.new_indices[elem[1]]][self.graph.new_indices[elem[0]]] == self.graph.graph_g[elem[1]][elem[0]]:
+                raise ValueError('Error: proof isomorphism')
