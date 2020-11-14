a2224aaed33509fb387d6c84df3bdf53d90e3da3
ZinkPavel <tsynk.p@gmail.com>
2020-11-14 17:24:41 +0700

    Fix main

--- hamiltonian_cycle/main.py
+++ hamiltonian_cycle/main.py
@@ -1,15 +1,21 @@
 from hamiltonian_cycle.graph import Graph
 from hamiltonian_cycle.crypt import GraphRSA
+from hamiltonian_cycle.swindler import Swindler
 
 
 def main():
     graph = Graph('data.txt')
-    graph.make_isomorphism_graph()
+    graph_rsa = GraphRSA(graph)
+    graph_rsa.encrypt()
 
-    cipher = GraphRSA(graph)
-    cipher.encrypt()
-    cipher.proof_isomorphism()
-    cipher.proof_cycle()
+    coded_graph = graph_rsa.coded
+    encrypted_graph = graph_rsa.encrypted
+
+    graph_rsa.proof_cycle()
+    graph_rsa.proof_isomorphism()
+
+    swindler = Swindler(graph)
+    swindler.fraud()
 
 
 if __name__ == "__main__":
